ca6dcd2fca8f3ef55e4b0f65d6fa146b4e9740aa
aashiq1460 <aashiqrana314@gmail.com>
2026-09-07 17:17:39 +0300

    Complete Express Assignment 1

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const app = express();
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+app.get('/api/v1/cats', (req, res) => {
+  const cat = {
+    cat_id: 1,
+    name: 'Mittens',
+    birthdate: '2022-05-15',
+    weight: 4.5,
+    owner: 'John Doe',
+    image: 'https://loremflickr.com/320/240/cat',
+  };
+
+  res.json(cat);
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
